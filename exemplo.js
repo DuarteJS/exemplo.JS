d32a8e9a323b6d7451648f3d9a24527417af210a
DuarteJS <gdmelo15@gmail.com>
2024-05-02 17:00:09 -0300

    feat: exemplo.JS

---
+++ exemplo.js
@@ -0,0 +1,22 @@
+// Escreva um programa que solicite o nome do usuario e lhe de boas vindas,
+// exibindo uma mensagem pelo console.
+
+//   prompt=mostrar mensagem
+// let nome= variavel que vai guardar no nome
+let nome = prompt("Ola,digite seu nome:")
+
+// "entao me mostre no console a variavel nome".
+// concatenacao (+) e para grudar dois nomes
+console.log("Seja Bem " + nome + "!!!");
+
+let pontos = 100
+            //parseInt() transforma oque esta entre () em numero inteiro.
+let bonus = parseInt ( prompt("Quantos pontos voce deseja somar?") )
+// parsefloat() tranforma o que esta () em numero com , (ponto/vigula flutuante).
+
+// Forma  de fazer somar
+console.log(pontos + bonus)
+
+let notaAluno = parseFloat(prompt("Digite a nota do aluno"))
+
+console.log(notaAluno);
---
+++ script.js
@@ -0,0 +1,5 @@
+alert("Hello World")
+
+// Ponto e virgula nao e necessario,porem pode ser usado ao minimizar o codigo
+console.log("Essa mensagem aparecera no console")
+
