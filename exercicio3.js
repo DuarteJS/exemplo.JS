656cf88ac563766a06ff2e3ad0da0d1f319b8939
DuarteJS <gdmelo15@gmail.com>
2024-05-03 15:15:13 -0300

    feat:exercicio3 concluido

---
+++ exercicio3.js
@@ -0,0 +1,11 @@
+// Faça um programa que receba um número do usuário e
+//  informe se este número é par ou ímpar.
+
+let n = parseInt(prompt("Digite um numero"))
+if (n%2 == 0){
+    console.log("O numero " + n + " é par")
+
+}else{
+    console.log("O numero " + n + " é impar")
+
+}
