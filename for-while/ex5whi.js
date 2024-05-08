29c07bb93e2a190c9280e50232069e2dda62e8c4
DuarteJS <gdmelo15@gmail.com>
2024-05-08 16:33:09 -0300

    feat:exercicio10

---
+++ for-while/ex5whi.js
@@ -0,0 +1,11 @@
+// Faça um programa que receba um número do usuário e continue
+// a pedir o número se o numero for diferente de 0. -->
+while (true) {
+
+    let n = prompt("Digite um número:")
+    if(n == "0"){
+     console.log("Digitou o numero 0")  
+      break
+    }
+    
+}
