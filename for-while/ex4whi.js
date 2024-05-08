3576ff5de4cf17840ddbc23cb30e491e315758cb
DuarteJS <gdmelo15@gmail.com>
2024-05-08 16:23:33 -0300

    feat:exercicio9

---
+++ for-while/ex4whi.js
@@ -0,0 +1,9 @@
+// . Faça um programa que receba um número do usuário e mostre
+// no console a contagem regressiva até 0.
+
+let n = prompt("Digite um numero: ")
+
+while (n >= 0){
+    console.log(n)
+    n--
+}
